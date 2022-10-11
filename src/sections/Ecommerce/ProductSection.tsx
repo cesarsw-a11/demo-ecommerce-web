53e86d783bef70248a0f3c9619d34bcb7071feeb
Cesar Meneses <cesarmeneses@localhost.localdomain>
2022-10-11 14:55:47 -0500

    Agregar carousel de productos

--- src/sections/Ecommerce/ProductSection.tsx
+++ src/sections/Ecommerce/ProductSection.tsx
@@ -2,6 +2,7 @@
 import ProductCard from '../../sections/Ecommerce/ProductCard';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
+import Carousel from 'react-material-ui-carousel';
 
 const productsItems_Women = ['product_1', 'product_2', 'product_3', 'product_4'];
 const productsItems_Men = ['product_5', 'product_6', 'product_7', 'product_8'];
@@ -13,18 +14,33 @@ export default function ProductSection() {
             <Typography variant='h3' sx={{ color: 'black', mt: 3, mb: 2, ml: 2 }}>
                 Women - New Arrivals
             </Typography>
-            <Grid container justifyContent='space-between' >
+            <Grid container justifyContent='space-between' sx={{ display: { sm: 'none', xs: 'block' } }} >
+                <Carousel>
+                    {productsItems_Women.map((item, key) => (
+                        <ProductCard key={key} productName={item} />
+                    ))}
+                </Carousel>
+            </Grid>
+            <Grid container justifyContent='space-between' sx={{ display: { sm: 'flex', xs: 'none' } }} >
                 {productsItems_Women.map((item, key) => (
+
                     <ProductCard key={key} productName={item} />
                 ))}
             </Grid>
             <Typography variant='h3' sx={{ color: 'black', mt: 3, mb: 2, ml: 2 }}>
                 Men - New Arrivals
             </Typography>
-            <Grid container justifyContent='space-between' marginBottom={5}>
-                {productsItems_Men.map((item, key) => (
-                    <ProductCard key={key} productName={item} />
-                ))}
+            <Grid container justifyContent='space-between' marginBottom={5} sx={{ display: { sm: 'none', xs: 'block' } }}>
+                <Carousel>
+                    {productsItems_Men.map((item, key) => (
+                        <ProductCard key={key} productName={item} />
+                    ))}
+                </Carousel>
+            </Grid>
+            <Grid container justifyContent='space-between' marginBottom={5} sx={{ display: { sm: 'flex', xs: 'none' } }}>
+                    {productsItems_Men.map((item, key) => (
+                        <ProductCard key={key} productName={item} />
+                    ))}
             </Grid>
         </>
     )
